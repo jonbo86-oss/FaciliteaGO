4e00bebf33792e44e7c11867fdac2b781aa50ebf
jonbo86-oss <jonbo86@gmail.com>
2026-04-29 18:29:05 +0200

    Mount LLM powered JuanIAs widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import MarketplaceExperienceV3 from "@/components/MarketplaceExperienceV3";
 import SearchButtonEnhancer from "@/components/SearchButtonEnhancer";
+import JuanIAsLLMWidget from "@/components/JuanIAsLLMWidget";
 
 export default function Home() {
   return (
     <>
       <MarketplaceExperienceV3 />
       <SearchButtonEnhancer />
+      <JuanIAsLLMWidget />
     </>
   );
 }
